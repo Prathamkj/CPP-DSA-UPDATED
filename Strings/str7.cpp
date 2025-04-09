6118539c57823ab2bdd7810030775ff8a6c59fa9
Pratham Jaulkar <prathamkj21@gmail.com>
2025-04-09 18:41:13 +0530

    commited message

---
+++ Strings/str7.cpp
@@ -0,0 +1,4 @@
+// if ((num[i] - '0') % 2 == 1 
+// let's understand meaning of this -> nums[i] - '0'
+// it means we are converting the character to integer
+// because num[i] -> is in form '3' and we are decreasing it by '0' so ---> it will be converted to 3
