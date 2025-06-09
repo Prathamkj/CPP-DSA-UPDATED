a9a014f809111f708c39cb6a845c5d74d1a53243
Pratham Jaulkar <prathamkj21@gmail.com>
2025-06-09 10:35:55 +0530

    Update v2.cpp with recent changes

--- Vector/v2.cpp
+++ Vector/v2.cpp
@@ -9,6 +9,9 @@ int main(){
         // isme i ki value index nhi hogi us indexx me jo value hai woh rahegi
         cout<<i<<endl;
     }
+    for(int j: v1){
+        cout<<j<<endl;
+    }
     // we can initialize vector 2 with the vector 1
     vector<int> v2(v1);   
     // so isko print karne se v1 hi print hoga
