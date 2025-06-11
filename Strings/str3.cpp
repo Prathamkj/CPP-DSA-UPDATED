be974a939c87829553021d54f41d556e3b6970ee
Pratham Jaulkar <prathamkj21@gmail.com>
2025-06-11 13:24:03 +0530

    string

--- Strings/str3.cpp
+++ Strings/str3.cpp
@@ -4,15 +4,52 @@ using namespace std;
 // 1910. Remove All Occurrences of a Substring
 
 //  string find() is a library function used to find the first occurrence of a sub-string in the given string
-// class Solution {
-// public:
-//     string removeOccurrences(string s, string part) {
-//         while(s.size()!=0 && s.find(part) < s.size()){
-//             s.erase(s.find(part),part.size());
-//         }
-//         return s;
-//     }
-// };
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.size() != 0 && s.find(part) < s.size()){
+            // Find the index where 'part' occurs in 's'
+            // Erase 'part' from 's' starting from that index
+            s.erase(s.find(part), part.size());
+        }
+        return s;
+    }
+};
+
+✅ Loop:
+🔹 Iteration 1: i = 0 → s[0] = 'm' → not space → continue
+🔹 Iteration 2: i = 1 → s[1] = 'y' → not space → continue
+🔹 Iteration 3: i = 2 → s[2] = ' ' → SPACE FOUND
+Now:
+
+cpp
+Copy
+Edit
+reverse(s.begin() + 0, s.begin() + 2);
+This reverses characters at index 0 to 1
+
+"my" → "ym"
+
+Now s = "ym code"
+Set start = 3 (next word starts after space)
+
+🔹 Iteration 4: i = 3 → s[3] = 'c'
+🔹 Iteration 5: i = 4 → s[4] = 'o'
+🔹 Iteration 6: i = 5 → s[5] = 'd'
+🔹 Iteration 7: i = 6 → s[6] = 'e'
+🔹 Iteration 8: i = 7 → END OF STRING (i == s.size())
+cpp
+Copy
+Edit
+reverse(s.begin() + 3, s.begin() + 7);
+
+
+
+
+
+
+
+
 
 // REVERSE WORDSS IN A STRING 
 
