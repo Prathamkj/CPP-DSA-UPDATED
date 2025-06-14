4942fbe5c2ebf8ff23bbce3d047bcddc39de3598
Pratham Jaulkar <prathamkj21@gmail.com>
2025-06-14 11:48:16 +0530

    chnges

--- Strings/str6.cpp
+++ Strings/str6.cpp
@@ -30,6 +30,34 @@ public:
         return ans;
     }
 };
+/// 
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        int count = 0;
+
+        for (char ch : s) {
+            if (ch == '(') {
+                if (count > 0) {
+                    ans += ch;
+                }
+                count++;
+            } else {
+                count--;
+                if (count > 0) {
+                    ans += ch;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+// ///
+
+
 
 
 Input: s = "[{()}]"
