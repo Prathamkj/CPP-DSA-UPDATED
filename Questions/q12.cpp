d1b0226f58263dd4deb74978fdae9716a38d2296
Pratham Jaulkar <prathamkj21@gmail.com>
2025-06-17 09:59:32 +0530

    Duplicates

--- Questions/q12.cpp
+++ Questions/q12.cpp
@@ -23,3 +23,29 @@ int main(){
     cout<<Duplicate(array,size);
 
 }
+
+442. Find All Duplicates in an Array
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) 
+    {
+        set<int> st;
+        vector<int> v;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(st.count(nums[i]))
+            {
+                //if element if present
+                v.push_back(nums[i]);
+            }
+            else
+            {
+                //if element is not present
+                st.insert(nums[i]);
+            }
+        }
+        return v;
+    }
+};
