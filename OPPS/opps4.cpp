b03dc6b6bf2f13e3623791fe54a9673255bddf04
Pratham Jaulkar <prathamkj21@gmail.com>
2025-06-20 21:25:06 +0530

    inheritance

--- OPPS/opps4.cpp
+++ OPPS/opps4.cpp
@@ -1,40 +1,58 @@
 // Inheritance in opps
 #include <iostream>
 using namespace std;
-class Person{
+
+class Person {
 public:
     string name;
     int age;
+
     // constructor
     // Person(string name, int age){
     //     this->name = name;
     //     this->age = age;
     // }
-    Person(){
 
+    Person() {
+        // default constructor
+    }
+
+    // to fix the constructor call in derived class
+    Person(string name, int age) {
+        this->name = name;
+        this->age = age;
     }
 };
+
 // inherit of class Person to class Student
 // Student class is derived class
 // Student class is child class
 // semicolon is important
-class Student : public Person{
+// So here student is a chile class and here it inherited the properties of the parent class and
+// it can acccess it also
+class Student : public Person {
 public:
     int rollno;
+
     // to isme roll no extra hai toh woh bhi print ho jayega
-    void getinfo(){
-        cout<<name<<endl;
-        cout<<age<<endl;
-        cout<<rollno<<endl;
+    void getinfo() {
+        cout << name << endl;
+        cout << age << endl;
+        cout << rollno << endl;
     }
 
     // for the paramaterized constructor
-    Student (string name, int age, int rollno) : Person(name, age){
+    Student(string name, int age, int rollno) : Person(name, age) {
         this->rollno = rollno;
+    }
+
+    // default constructor (needed to create object like Student p1;)
+    Student() {
+        // default constructor
     }
 };
 
-int main(){
+int main() {
     Student p1;
     p1.name = "Pratham";
     p1.age = 21;
