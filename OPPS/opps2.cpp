b05dbe70fcba5571e40a3d1373a7f77b8f26ee74
Pratham Jaulkar <prathamkj21@gmail.com>
2025-06-20 21:10:58 +0530

    this

--- OPPS/opps2.cpp
+++ OPPS/opps2.cpp
@@ -22,6 +22,8 @@ public:
     // paramaterized constructor
     Teacher(string n, string d, string s){
         this->name = name;
+        // this is a special pointer that points to the cuurrennt object 
+        // to specify the paramater wht it is 
         // object ka name hai
         // isse syntactialy correct hai
         dept = d;
