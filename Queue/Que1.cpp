073108b850f1129a07b7c49424864039b5910871
Pratham Jaulkar <prathamkj21@gmail.com>
2025-06-28 11:48:36 +0530

    Add Queue file

---
+++ Queue/Que1.cpp
@@ -0,0 +1,89 @@
+// IMPLEMENTATION OF QUEUE USING ARRAY
+
+#include<bits/stdc++.h>
+
+using namespace std;
+class Queue {
+  int * arr;
+  int start, end, currSize, maxSize;
+  public:
+    Queue() {
+      arr = new int[16];
+      // array
+      // assiggning the start and the end to -1
+      start = -1;
+      end = -1;
+      currSize = 0;
+    }
+
+  Queue(int maxSize) {
+    ( * this).maxSize = maxSize;
+    arr = new int[maxSize];
+    start = -1;
+    end = -1;
+    currSize = 0;
+  }
+  // for push of the Element
+  void push(int newElement) {
+    if (currSize == maxSize) {
+      // if queue is full then return nothing
+      cout << "Queue is full\nExiting..." << endl;
+      exit(1);
+    }
+    // if your end = -1 
+    // so you need to now start your both the pointers 
+    if (end == -1) {
+      start = 0;
+      end = 0;
+    } 
+    // if your end is at the last position then you have to fix back to i'th position 
+    // using rotation 
+    else
+      end = (end + 1) % maxSize;
+    arr[end] = newElement;
+    cout << "The element pushed is " << newElement << endl;
+    currSize++;
+  }
+  int pop() {
+    if (start == -1) {
+      cout << "Queue Empty\nExiting..." << endl;
+    }
+    // assinning the fist value
+    int popped = arr[start];
+    if (currSize == 1) {
+      start = -1;
+      end = -1;
+    } 
+    // do the same shit for start
+    else
+      start = (start + 1) % maxSize;
+    currSize--;
+    return popped;
+  }
+  int top() {
+    if (start == -1) {
+      cout << "Queue is Empty" << endl;
+      exit(1);
+    }
+    return arr[start];
+  }
+  int size() {
+    return currSize;
+  }
+
+};
+
+int main() {
+  Queue q(6);
+  q.push(4);
+  q.push(14);
+  q.push(24);
+  q.push(34);
+  cout << "The peek of the queue before deleting any element " << q.top() << endl;
+  cout << "The size of the queue before deletion " << q.size() << endl;
+  cout << "The first element to be deleted " << q.pop() << endl;
+  cout << "The peek of the queue after deleting an element " << q.top() << endl;
+  cout << "The size of the queue after deleting an element " << q.size() << endl;
+
+  return 0;
+}
