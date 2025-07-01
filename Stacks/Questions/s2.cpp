4e9d81da414714f342c495cc0384c8f729a55281
Pratham Jaulkar <prathamkj21@gmail.com>
2025-07-01 11:47:37 +0530

    vector

--- Stacks/Questions/s2.cpp
+++ Stacks/Questions/s2.cpp
@@ -26,3 +26,30 @@ public:
         return ans;
     }
 };
+
+#include <iostream>
+// using namespace std;
+// #include <stack>
+// #include <vector>
+// int main(){
+
+//     vector<int> arr = {6,8,0,1,3};
+//     stack<int> st;
+//     vector<int> ans;
+//     int n = arr.size();
+//     for(int i = n -1; i>=0; i--){
+//         while(!st.empty() && st.top() <= arr[i]){
+//             st.pop();
+//         }
+//         if(st.empty()){
+//             ans.push_back(-1);
+//         }else{
+//             ans.push_back(st.top());
+//         }
+//         st.push(arr[i]);
+//     }
+//     for(int i = 0; i < n; i++){
+//         cout << ans[i] << " ";
+
+// USING UNORDERED MAP
+
