e4e8bee245912fbe969c31633062dcb385668d88
Pratham Jaulkar <prathamkj21@gmail.com>
2025-08-31 22:29:37 +0530

    Merge sort

--- Sorting/MergeSort.cpp
+++ Sorting/MergeSort.cpp
@@ -2,42 +2,51 @@
 using namespace std;
 // MergeSort 
 // using Recursion
+// just use divide and merge
+// and before merging sort the arrays
 void merge(int *arr, int s, int e) {
 
-    int mid = (s+e)/2;
-
+    int mid = (s+e)/2; 
+    // taking the mid here
     int len1 = mid - s + 1;
+    // first half array ki length
     int len2 = e - mid;
+    // second half array ki length
+
     // yaha humne 2 alag alag array banayee
     int *first = new int[len1];
     int *second = new int[len2];
-
-    //copy values
+    // creating a 2 arrays first and second
+    //and copy values
     // first half ki values ko first array mei copy karte hai
     int mainArrayIndex = s;
+    // start se mid tak ki value copy karna start kar diyee
     for(int i=0; i<len1; i++) {
         first[i] = arr[mainArrayIndex++];
+        // first half ki values ko first array mei copy karte hai
     }
     // second array mid+1 se start hota hai
     mainArrayIndex = mid+1;
+    // for 2nd array and len2
     for(int i=0; i<len2; i++) {
         second[i] = arr[mainArrayIndex++];
     }
-
-    //merge 2 sorted arrays     
+    // now we apply merge 2 sorted arrays
+    //merge 2 sorted arrays
     int index1 = 0;
     int index2 = 0;
     mainArrayIndex = s;
     // main array ka index starting se hai
     while(index1 < len1 && index2 < len2) {
         if(first[index1] < second[index2]) {
+            // if first is less than 2nd then start wala part first array me dal do and increment kar do
             arr[mainArrayIndex++] = first[index1++];
         }
         else{
             arr[mainArrayIndex++] = second[index2++];
         }
     }   
-
+    // for each one remaining in either array
     while(index1 < len1) {
         arr[mainArrayIndex++] = first[index1++];
     }
@@ -54,6 +63,7 @@ void merge(int *arr, int s, int e) {
 void mergeSort(int *arr , int s , int e){
     // base case
     if(s>=e){
+        // start end se aage hai to kuch nhi karna
         return;
     }
     int mid = (s+e)/2;
@@ -77,6 +87,7 @@ int main() {
 
     for(int i=0;i<n;i++){
         cout << arr[i] << " ";
+        // print kar diya array ko
     } cout << endl;
 
     return 0;
