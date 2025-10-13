f0f74def0b36a6f123449a7f50b5d6f6e131841e
Pratham Jaulkar <prathamkj21@gmail.com>
2025-10-13 12:08:25 +0530

    git push

---
+++ Array/A22.cpp
@@ -0,0 +1,46 @@
+78. Subsets
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Approach 1 
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans = {{}};
+
+        for (int num : nums) {
+            int size = ans.size();
+            for (int i = 0; i < size; i++) {
+                vector<int> subset = ans[i];
+                subset.push_back(num);
+                ans.push_back(subset);
+            }
+        }
+        return ans;
+    }
+};
+
+
+Approach 2 
+Using Bit Manipulation
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        int total = 1 << n; // 2^n subsets
+        for (int mask = 0; mask < total; mask++) {
+            vector<int> subset;
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) {
+                    subset.push_back(nums[i]);
+                }
+            }
+            ans.push_back(subset);
+        }
+        return ans;
+    }
+};
