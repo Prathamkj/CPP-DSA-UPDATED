2232aa7c8ea4a687f93fe97fc154e436f74955bd
Pratham Jaulkar <prathamkj21@gmail.com>
2025-11-25 16:52:45 +0530

    LtiMindtree

---
+++ Questions/q35.cpp
@@ -0,0 +1,25 @@
+Amstrong Number 
+#include <iostream>
+using namespace std;
+// cube karne ke baad bhi whi number same aaya 
+int main() {
+    int num, temp, sum = 0, digit;
+
+    cout << "Enter number: ";
+    cin >> num;
+
+    temp = num;
+
+    while (temp > 0) {
+        digit = temp % 10;      // get last digit
+        sum += digit * digit * digit;  // cube it
+        temp = temp / 10;       // remove last digit
+    }
+
+    if (sum == num)
+        cout << "Armstrong Number";
+    else
+        cout << "Not Armstrong Number";
+
+    return 0;
+}
