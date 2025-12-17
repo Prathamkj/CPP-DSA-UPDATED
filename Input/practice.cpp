db8eee8c24ae42f23fd3a03e542ca9156a4f96a1
Pratham Jaulkar <prathamkj21@gmail.com>
2025-12-17 12:51:56 +0530

    Tcs practice

--- Input/practice.cpp
+++ Input/practice.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 #include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str;
+    getline(cin , str);
+    stringstream ss(str);
+    vector<int> arr;
+    string temp;
+    while(getline(ss, temp, ',')){
+        stringstream val(temp);
+        int num;
+        val >> num;
+        arr.push_back(num);
+    }
+
+
+    string str;
+    getline(cin, str);
+    stringstream s
+}
